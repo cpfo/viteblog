b919fbe80e015e8c2486e4c50938108c2a0f1977
chenpengfei <346798132@qq.com>
2026-06-14 14:27:11 +0800

    feat: integrate Waline comment system

---
+++ docs/.vitepress/theme/components/Waline.vue
@@ -0,0 +1,13 @@
+<template>
+  <Waline :serverURL="serverURL" :path="path" />
+</template>
+<script setup lang="ts">
+import { Waline } from '@waline/client/component'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+import '@waline/client/style'
+
+const serverURL = 'https://hexo-comments-weld.vercel.app/'
+const path = computed(() => useRoute().path)
+</script>
